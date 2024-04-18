ad1322fd24780de67f914efdd274492aa4499c02
Praveen kumar Bandi <167025759+chinnollasrujana@users.noreply.github.com>
2024-04-18 04:36:49 -0500

    updted files

--- controller.js
+++ controller.js
@@ -1,32 +1,13 @@
 const url = require('url');
-// const foodItems = require('./userData.js');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://chinnollasrujana1011:Srujana1109@indianrestaurants.7jajvkx.mongodb.net/?retryWrites=true&w=majority&appName=indianrestaurants";
-const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-
-exports.getFoodItems = async function(req, res) {
-    
-    const reqUrl = url.parse(req.url, true)
-    await client.connect();
-    const cursor = client.db("sample_mflix").collection("embedded_movies").find({});
-   const results = await cursor.toArray();
-   console.log(results);
-   let foodList = (JSON.stringify(results));
-   console.log(foodList, 'hyd');
-    
-   await client.close();
-
+const foodList = require('./userData.js');
+exports.getFoodItems = function(req, res) {
+  const reqUrl = url.parse(req.url, true)
   var response = [
     {
-      "message": "Here are the list of food list "
+      "message": "Here are the list of Food List "
     },
-    foodList
+   // users,
+   foodItems
   ];
   res.statusCode = 200;
   res.setHeader('content-Type', 'Application/json');
--- userData.js
+++ userData.js
@@ -1,16 +1,59 @@
 let foodList;
+
+exports.exports = users =[
+
+  {
+    id: 1,
+    name: 'Seyi',
+    location: 'Buenos Aires'
+  },
+  {
+    id: 2,
+    name: 'David Khan',
+    location: 'Argentina'
+  },
+  {
+    id: 3,
+    name: 'Isaac',
+    location: 'Kwala lumpor'
+  }
+ ];
+
+
+
+
+
 // const {MongoClient} = require('mongodb');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://chinnollasrujana1011:Srujana1109@indianrestaurants.7jajvkx.mongodb.net/?retryWrites=true&w=majority&appName=indianrestaurants";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+exports.exports = foodItems = async () => {
+  await client.connect()
+  const foods = client.db("sample_mflix").collection("users")
+  const one = await foods.findOne()
+  //console.log(one)
+  const cursor = client.db("sample_mflix").collection("users").find({});
+   const results = await cursor.toArray();
+   foodList = (JSON.stringify(results));
+   return foodList;
+   console.log(foodList, 'hyd');
+   
+}
+
+// foodItems()
+
+
+
 
 exports.foodItems = async function() {
-const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
+    
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
@@ -18,7 +61,6 @@ const client = new MongoClient(uri, {
    // await client.db("sample_mflix").command({ ping: 1 });
    const cursor = client.db("sample_mflix").collection("embedded_movies").find({});
    const results = await cursor.toArray();
-   console.log(results);
    foodList = (JSON.stringify(results));
    console.log(foodList, 'hyd');
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -27,4 +69,3 @@ const client = new MongoClient(uri, {
     await client.close();
   }
 }
-
