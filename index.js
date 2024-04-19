c725e445a87644c76f8a5fc0664c2ebe86a103cf
Praveen kumar Bandi <167025759+chinnollasrujana@users.noreply.github.com>
2024-04-19 01:08:59 -0500

    updted files

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const http = require('http');
 const hostname = '0.0.0.0';
-const port = 3000;
-const server = require('./route.js'); // imports the routing file
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
 
-server.listen(process.env.PORT || 3000 );
+const PORT = 3000;
+const server = require('./route.js'); // imports the routing file
+server.listen(PORT, hostname, () => {
+  console.log(`Server running at http://${hostname}:${PORT}/`);
+});
+
+//server.listen(process.env.PORT || 3000 );
