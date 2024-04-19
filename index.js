1ee39e8dc5201f79a07787f8457c9947fc575027
Praveen kumar Bandi <167025759+chinnollasrujana@users.noreply.github.com>
2024-04-19 00:10:09 -0500

    Initial commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const http = require('http');
 const hostname = '0.0.0.0';
 const port = 3000;
 const server = require('./route.js'); // imports the routing file
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+server.listen(process.env.PORT || 3000 );
